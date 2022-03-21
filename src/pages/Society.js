093a04dfa5c8cc72faaa5bfd76b127252e84f8eb
rani <rj5150494@gmail.com>
2022-03-21 11:10:36 +0530

    add style link in index

--- src/pages/Society.js
+++ src/pages/Society.js
@@ -3,15 +3,7 @@ import React from "react";
 function Society() {
   return (
     <>
-      <style>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossOrigin="anonymous"
-        />
-        
-      </style>
+      
       
       <form style={{}}>
         <div className="mb-3 " style={{}}>
